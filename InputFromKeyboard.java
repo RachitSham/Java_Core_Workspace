2986654d3c44cdbfeb709aecc0e8d42858fd1e14
Rachit Sharma Workspace <144218247+RachitSham@users.noreply.github.com>
2024-01-10 20:33:20 +0530

    Add files via upload

---
+++ InputFromKeyboard.java
@@ -0,0 +1,17 @@
+import java.util.*;
+/*
+* Entered The String By An User .
+*/
+class InputFromKeyboard
+{
+	public static void main(String[] args)
+	{
+	//creating scanner class to read input from keyboard .
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please Enter The String :-");
+		String s = sc.nextLine();
+		
+		System.out.println("String Entered"+ " ");
+	}
+}
