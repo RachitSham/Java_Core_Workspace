7767a74a2b85f9e4211bff40ce4aa762c0184327
Rachit Sharma Workspace <144218247+RachitSham@users.noreply.github.com>
2024-01-11 14:37:33 +0530

    Add files via upload

---
+++ TableReverse.java
@@ -0,0 +1,33 @@
+import java.util.*;
+/*
+*To print the table reverse.
+*/
+public class TableReverse
+{
+	/*
+	*this method is used to print the table reverse.
+	*@Parameter : [s] is used for starting table number , and [n] is used for ending table number.
+	*@Return : no return type.
+	*/
+	public void reverse(int s)
+	{
+		for(int i=10 ;i>=1 ;i--)
+		{
+		 int r = s*i;
+		 System.out.println(s+" * " + i +" = "+r);
+		}
+			
+	}
+	public static void main(String[] args)
+	{
+		// creating Scanner class to read input from keyboard.
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please Enter The Starting table Number :-");
+		int s = sc.nextInt();
+		
+  		// creating table objects to call natural method.
+		TableReverse tr1 = new TableReverse();
+		tr1.reverse(s);
+	}
+}
