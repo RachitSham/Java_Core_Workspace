9ba9f6c963f9681fa47c077ce15f4c704f2e7a04
Rachit Sharma Workspace <144218247+RachitSham@users.noreply.github.com>
2024-01-11 15:10:30 +0530

    Add files via upload

---
+++ ReverseAlphabet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+/*
+*To print reverse alphabets from Z to A.
+*/
+class ReverseAlphabet
+{
+	public static void main(String[] args)
+	{
+		for(char i='Z'; i>='A'; i--)
+		{
+			System.out.println(i);
+		}
+	} 
+}
