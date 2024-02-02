dbe934b4fce386cf1299e4dd011c0e563cb9c7fe
Rachit Sharma Workspace <144218247+RachitSham@users.noreply.github.com>
2024-02-02 11:55:09 +0530

    Add files via upload

---
+++ LogicalOperator/Logical_AND_Demo.java
@@ -0,0 +1,17 @@
+// This Program Demonstrate to Logical AND Operator..//
+
+public class Logical_AND_Demo
+{
+	public static void main(String[] args)
+	{
+	    int r = 6;
+		int h = 50;
+		
+		boolean b = (r > 3 && h < 30);
+		
+		// returns false because 6 is greater than 3 AND 50 is also greater than 30.
+
+		System.out.println(b);
+		
+	}
+}
---
+++ LogicalOperator/Logical_OR_Demo.java
@@ -0,0 +1,17 @@
+// This Program Demonstrate to Logical OR Operator..//
+
+public class Logical_OR_Demo
+{
+	public static void main(String[] args)
+	{
+	    int a = 6;
+		int c = 45;
+		
+		boolean b = (a > 3 || c < 50);
+		
+		// returns false because 6 is greater than 3 OR 45 is also lesser than 50.
+		
+		System.out.println(b);
+		
+	}
+}
